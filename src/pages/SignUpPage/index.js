5aade4751f7db8f6de2942ef4b3a9d8bd0720d0b
Conor <cgaughan241@gmail.com>
2021-01-06 21:26:01 -0500

    added form that redirects to dashboard

--- src/pages/SignUpPage/index.js
+++ src/pages/SignUpPage/index.js
@@ -1,9 +1,68 @@
+import { useState } from 'react';
 
+function SignupPage(props) {
+    const [formState, setFormState] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    })
 
+    function getInitialFormState() {
+        return {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: ""
+        }
+    }
 
-function SignupPage() {
+    function handleChange(event) {
+        setFormState(prevState => ({
+            ...prevState,
+            [event.target.name]: event.target.value
+        }))
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('submitted form data', formState);
+        setFormState(getInitialFormState)
+        props.history.push('/dashboard')
+    }
+
     return (
-        <div>SignupPage</div>
+        <div className="Page">
+            <form onSubmit={handleSubmit}>
+                <input 
+                value={formState.firstName} 
+                onChange={handleChange} 
+                name="firstName" 
+                type='text'>
+                </input>
+                <input 
+                value={formState.lastName} 
+                onChange={handleChange} 
+                name="lastName" 
+                type='text'>  
+                </input>
+                
+                <input 
+                value={formState.email} 
+                onChange={handleChange} 
+                name="email" 
+                type='email'>
+                </input>
+                
+                <input 
+                value={formState.password} 
+                onChange={handleChange} 
+                name="password" 
+                type='password'>
+                </input>
+                <button>Sign up</button>
+            </form>
+        </div>
     )
 }
 
