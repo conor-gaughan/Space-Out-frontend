c418e6307d0310b4a92db530ec4ed44e7fe54afa
Conor <cgaughan241@gmail.com>
2021-01-16 12:38:07 -0500

    added rocket detail information

--- src/pages/RocketDetailPage/RocketDetailPage.js
+++ src/pages/RocketDetailPage/RocketDetailPage.js
@@ -1,10 +1,20 @@
-
+import styles from './RocketDetailPage.module.scss'
 
 function RocketDetailPage(props) {
+    
     return (
         <>
         <h1>Rocket Detail Page</h1>
-        {props.rocket.name}
+        <p style={{color: 'red'}}>Consider a table</p>
+        <p>Name: {props.rocket.name}</p>
+        <p>Description: {props.rocket.description}</p>
+        <p>Active: {props.rocket.active}</p>
+        <p>Engine: {props.rocket.engines.type}</p>
+        <p>Height: {props.rocket.height.feet}</p>
+        <p>Cost per launch: {props.rocket.cost_per_launch}</p>
+        <p>First flight: {props.rocket.first_flight}</p>
+
+        
         </>
     )
 }
