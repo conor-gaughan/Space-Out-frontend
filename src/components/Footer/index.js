18871c075f09d06da0ad74179d87e962b11b2b81
Conor <cgaughan241@gmail.com>
2021-01-19 22:06:03 -0500

    added react icons and footer social icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,19 @@
 import styles from './Footer.module.css'
+import { Link } from 'react-router-dom'
 
-
+import { FaGithubSquare } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 
 function Footer() {
     return (
         <div className={styles.Footer}>
-        <p>Footer {new Date().getFullYear()}</p>
+            <Link to='https://www.linkedin.com/in/conorgaughan/'>
+                <p styles={{margin: '10px'}}><FaGithubSquare size={60} /></p>
+            </Link>
+            <Link to='https://github.com/conor-gaughan'>
+                <FaLinkedin size={60} />
+            </Link>
+        {/* <p> {new Date().getFullYear()} </p> */}
         {/* <img style={{backgroundColor: 'red'}}src={github}></img> */}
         </div>
     )
