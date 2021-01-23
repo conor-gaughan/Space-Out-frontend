dca8312e5ea67b5572a6eeeb22ae95159126c001
Conor <cgaughan241@gmail.com>
2021-01-23 11:25:44 -0500

    added image to roadster page

--- src/pages/RoadsterPage/index.js
+++ src/pages/RoadsterPage/index.js
@@ -7,6 +7,7 @@ function RoadsterPage(props) {
             <p>{props.roadsterData.details}</p>
             <p>Current Speed: {props.roadsterData.speed_mph.toFixed(2)} mph</p>
             <p>Distance from Earth: {props.roadsterData.earth_distance_mi.toFixed(2)} miles</p>
+            <img src={props.roadsterData.flickr_images[0]}></img>
         </div>
     )
 }
